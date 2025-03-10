2f495af68aee10bee2a8a7ff23ecc12eacff92a8
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-10 13:13:16 -0500

    feat: removing a todo item with a removeTodoHandler function executed by clicking a button with onClick.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -4,11 +4,18 @@ import TodoItem from "./TodoItem";
 
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
+const Todos: React.FC<{ items: Todo[]; onRemove: (id: string) => void }> = ({
+  items,
+  onRemove,
+}) => {
   return (
     <ul className={classes.todos}>
       {items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={onRemove.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
