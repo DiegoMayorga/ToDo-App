4f6382d6059fcd38b91cf47b3793fe60d3c9e5d8
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-10 12:23:15 -0500

    feat: Using useState to update the todo list using Todo[] to specify the data type.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const addTodoHandler = (todoText: string) => {
-    return todoText;
-  }
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
